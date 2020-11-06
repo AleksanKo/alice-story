329a061c84d9995f1e07111495d57f7795c1b06e
AleksanKo <koalasaxen@gmail.com>
2020-11-06 22:27:56 +0200

    editing functions

--- app.py
+++ app.py
@@ -3,37 +3,49 @@ import re
 from alice_scripts import Skill, request, say, suggest
 skill = Skill(__name__)
 
-def replace_tags(content):
-    return content.replace('<p>','').replace('</p>','')
+def replace_tags_and_brackets(content):
+    filtered_content = content.replace('<p>', '').replace('</p>', '')\
+                                .replace('&lt;', '').replace('&gt;', '').replace('<br>', '')\
+                                .replace('speaker audio=','')
+
+    filtered_content = re.sub(r'\".*\"', '', filtered_content)
+    print(filtered_content)
+    return filtered_content
 
 def get_children_buttons(node):
     buttons = []
     for i in elements[node]['outputs']:
-        buttons.append(replace_tags(connections[i]['label']))
+        buttons.append(replace_tags_and_brackets(connections[i]['label']))
     return buttons
 
 def search_for_audio(content):
-    pass
+    try:
+        audio_link = re.search(r'\"(.*)\"', content).group(1)
+        print(audio_link)
+    except AttributeError:
+        audio_link = ''
+    return audio_link
 
 def get_children_connections(node):
     children_connections = {}
     #print(elements[node]['outputs'])
     for i in elements[node]['outputs']:
         try:
-            children_connections.update({replace_tags(connections[i]['label']):
+            children_connections.update({replace_tags_and_brackets(connections[i]['label']):
                                              jumpers[connections[i]['targetid']]['elementId']})
         except KeyError:
-            children_connections.update({replace_tags(connections[i]['label']): connections[i]['targetid']})
+            children_connections.update({replace_tags_and_brackets(connections[i]['label']): connections[i]['targetid']})
     print(children_connections)
     return children_connections
 
 with open('project_settings.json', 'r') as p:
     all_data = json.loads(p.read())
+
 starting_element = all_data['startingElement']
 connections = all_data['connections']
 elements = all_data['elements']
 jumpers = all_data['jumpers']
-beginning = replace_tags(elements[starting_element]['content'])
+beginning = replace_tags_and_brackets(elements[starting_element]['content'])
 #print(elements[starting_element]['content'])
 #print(elements[connections[elements[starting_element]['outputs'][0]]['targetid']]['title'])
 #print(beginning['outputs'])
@@ -49,23 +61,20 @@ def run_script():
     children_connections = get_children_connections(starting_element)
     #print(children_connections)
     #print(elements[children_connections[following_element]])
-    #в цикл это все
 
-    yield say(replace_tags(elements[children_connections[following_element]]['content']),
+    yield say(replace_tags_and_brackets(elements[children_connections[following_element]]['content']),
               suggest(*get_children_buttons(children_connections[following_element])))
 
-    while children_connections :
+    while children_connections:
         children_connections = get_children_connections(children_connections[following_element])
         following_element = request.command
         #print(elements[children_connections[following_element]]['content'])
-        yield say(replace_tags(elements[children_connections[following_element]]['content']),
+        yield say(replace_tags_and_brackets(elements[children_connections[following_element]]['content']),
                   suggest(*get_children_buttons(children_connections[following_element])),
-                  tts='alice-sounds-things-door-2.opus')
+                  tts=search_for_audio(elements[children_connections[following_element]]['content']))
 
-    #yield say('Сколько вам лет?')
     #while not request.matches(r'\d+'):
         #yield say('Я вас не поняла. Скажите число')
-    #age = int(request.command)
 
     yield say('Рада',
               end_session=True)
